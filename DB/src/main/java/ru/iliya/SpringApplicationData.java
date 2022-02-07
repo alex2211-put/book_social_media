c05432ab67f1ff0fccdb78f8a2554747efe87040
Riernish <osipov.ia@phystech.edu>
2022-02-07 23:29:43 +0300

    problems to check

--- DB/src/main/java/ru/iliya/SpringApplicationData.java
+++ DB/src/main/java/ru/iliya/SpringApplicationData.java
@@ -1,13 +1,22 @@
 package ru.iliya;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import ru.iliya.repositories.AuthorRepository;
+import ru.iliya.repositories.BaseRepository;
+import ru.iliya.repositories.BookRepository;
+
+import javax.validation.constraints.Email;
 
 @SpringBootApplication
-@ImportResource({"classpath:context.xml"})
 public class SpringApplicationData {
+
+    @Autowired
+    BaseRepository baseRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringApplicationData.class, args);
