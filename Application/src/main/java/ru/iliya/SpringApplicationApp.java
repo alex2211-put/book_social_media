1524557314a765b3344571ad9e2a4a6f70beb6d3
Riernish <osipov.ia@phystech.edu>
2022-02-19 20:22:35 +0300

    second attempt to complete one service

--- Application/src/main/java/ru/iliya/SpringApplicationApp.java
+++ Application/src/main/java/ru/iliya/SpringApplicationApp.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import ru.iliya.entities.Author;
+import ru.iliya.repositories.BookRepository;
 
 
 @SpringBootApplication
@@ -15,15 +16,21 @@ public class SpringApplicationApp {
     @Autowired
     SpringApplicationData springApplicationData;
 
+    @Autowired
+    BookRepository bookRepository;
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringApplicationApp.class, args);}
+        SpringApplication.run(SpringApplicationApp.class, args);
+    }
 
     @Bean
     public CommandLineRunner springdata() {
         return(args) -> {
 
             System.out.println("-------StartTest--------");
-            System.out.println(bookSearchService.findBooksByGenre("Русские детективы"));
+            //System.out.println(bookSearchService.findBooksByGenre("Русские детективы"));
+            System.out.println(bookSearchService.findBookByTitle("Часы Зигмунда Фрейда"));
+            System.out.println(bookSearchService.findBooksByAuthor("Александр", "Левитас"));
 
             System.out.println("----------end-----------");
         };
