6b1bdbfac706da66f493236850daf846b59e7522
RomanMikhailovDASR <ziroxxx3@gmail.com>
2022-03-13 23:27:14 +0300

    fix

--- DB/src/main/java/ru/iliya/repositories/MongoMessageRepository.java
+++ DB/src/main/java/ru/iliya/repositories/MongoMessageRepository.java
@@ -1,11 +1,11 @@
-//package ru.iliya.repositories;
-//
-//import ru.iliya.entities.Message;
-//
-//import java.util.Optional;
-//
-//public interface MongoMessageRepository extends org.springframework.data.mongodb.repository.MongoRepository<Message, String> {
-//
-//    public Optional<Message> findById(String firstName);
-//
-//}
+package ru.iliya.repositories;
+
+import ru.iliya.entities.Message;
+
+import java.util.Optional;
+
+public interface MongoMessageRepository extends org.springframework.data.mongodb.repository.MongoRepository<Message, String> {
+
+    public Optional<Message> findById(String firstName);
+
+}
