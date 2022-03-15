cef4bed2007c8a713ad21becc43b42387e6b2189
Alexander Putin <alik-put@yandex-team.ru>
2022-03-15 21:04:40 +0300

    fix

--- Application/src/main/java/ru/iliya/BookSearchController.java
+++ Application/src/main/java/ru/iliya/BookSearchController.java
@@ -10,6 +10,7 @@ import ru.iliya.repositories.BaseRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class BookSearchController {
@@ -18,6 +19,7 @@ public class BookSearchController {
     BookSearchService bookSearchService;
 
     String title;
+
     @GetMapping("/book/search") //book/search
     public String showBooksByTitle(@RequestParam(name = "title", required = false, defaultValue = "") String title,
                                    @RequestParam(name = "name", required = false, defaultValue = "") String name,
@@ -28,7 +30,7 @@ public class BookSearchController {
         return "book-by-title"; //view
     }
 
-//    @GetMapping("/book-by-title")
+    //    @GetMapping("/book-by-title")
 //    public String searchBookByTitle(@RequestParam(name = "title") String title) {
 //        this.title = title;
 //
@@ -36,10 +38,23 @@ public class BookSearchController {
 //    }
     @GetMapping("/book/info")
     public String showBookInfo(@RequestParam(name = "book_id", required = false, defaultValue = "6047063") String book_id,
+                               @RequestParam(name = "favourites", required = false, defaultValue = "Add to favourites") String favourites,
                                Model model) {
         model.addAttribute("book",
                 bookSearchService.findBookById(book_id));
+        model.addAttribute("favourites", favourites);
         return "book";
     }
 
+    @PostMapping("/book/info/addFavourites/{book_id}/{favourites}")
+    public String addFavourites(@PathVariable(name = "book_id") String book_id,
+                                @PathVariable(name = "favourites") String favourites,
+                                Model model) {
+        if (Objects.equals(favourites, "Add to favourites")) {
+            favourites = "Remove from favourites";
+        } else {
+            favourites = "Add to favourites";
+        }
+        return showBookInfo(book_id, favourites, model);
+    }
 }
