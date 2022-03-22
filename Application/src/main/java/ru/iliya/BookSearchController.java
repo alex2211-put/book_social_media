7c393e60369872fb8605a9c508673005c5ed2d83
Alexander Putin <alik-put@yandex-team.ru>
2022-03-22 20:20:24 +0300

    fix

--- Application/src/main/java/ru/iliya/BookSearchController.java
+++ Application/src/main/java/ru/iliya/BookSearchController.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ru.iliya.entities.Comments;
 import ru.iliya.services.BookSearchService;
 import ru.iliya.repositories.BaseRepository;
 
+import javax.xml.stream.events.Comment;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -40,6 +42,8 @@ public class BookSearchController {
     public String showBookInfo(@RequestParam(name = "book_id", required = false, defaultValue = "5944307") String book_id,
                                @RequestParam(name = "favourites", required = false, defaultValue = "Add to favourites") String favourites,
                                Model model) {
+        List<Comments> comments = bookSearchService.getComments(book_id);
+        model.addAttribute("comments", comments);
         model.addAttribute("book",
                 bookSearchService.findBookById(book_id));
         model.addAttribute("favourites", favourites);
@@ -56,5 +60,13 @@ public class BookSearchController {
             favourites = "Add to favourites";
         }
         return showBookInfo(book_id, favourites, model);
+    }
+
+    @PostMapping("/book/info/addComment/{book_id}")
+    public String addComment(@PathVariable(name = "book_id") String book_id,
+                             @RequestParam(name = "comment") String comment,
+                                Model model) {
+        bookSearchService.addComment(book_id, comment);
+        return "redirect:/book/info?book_id=" + book_id;
     }
 }
