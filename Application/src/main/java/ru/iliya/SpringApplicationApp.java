078f5af6de99d4e6c48099f931bacba27af76cdd
RomanMikhailovDASR <ziroxxx3@gmail.com>
2022-03-29 12:55:07 +0300

    user view with recommendations2

--- Application/src/main/java/ru/iliya/SpringApplicationApp.java
+++ Application/src/main/java/ru/iliya/SpringApplicationApp.java
@@ -5,16 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import ru.iliya.entities.Book;
-import ru.iliya.entities.Message;
-import ru.iliya.repositories.BaseRepository;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import ru.iliya.repositories.BookRepository;
-import ru.iliya.repositories.FriendsRepository;
-import ru.iliya.services.BookSearchService;
-import ru.iliya.services.MarksService;
-import ru.iliya.services.MessageService;
-
-import java.util.List;
+import ru.iliya.services.*;
 
 
 @SpringBootApplication
@@ -28,7 +21,7 @@ public class SpringApplicationApp {
     RecommendationsService recommendationsService;
     @Autowired
     BlockedUsersService blockedUsersService;
-//    CommentService commentService;
+    //    CommentService commentService;
     @Autowired
     UserServiceImpl userService;
     @Autowired
@@ -39,10 +32,6 @@ public class SpringApplicationApp {
     MessageService messageService;
     @Autowired
     BookRepository bookRepository;
-    @Autowired
-    FriendsRepository friendsRepository;
-    @Autowired
-    BaseRepository baseRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringApplicationApp.class, args);
@@ -51,15 +40,11 @@ public class SpringApplicationApp {
     @Bean
     public CommandLineRunner springdata() {
         return (args) -> {
-            System.out.println("-------StartTest--------");
-            System.out.println("adm1nadm!n".hashCode());
-            System.out.println("!devel0per".hashCode());
-            System.out.println("юзерИзКит@я".hashCode());
-
-//            baseRepository.setFriendsByUserIDAndUser2ID(1, 5);
-            System.out.println(friendsRepository.findByUserIDAndUser2ID(1, 5));
-//            baseRepository.deleteFriendsByFriendID(3);
-            System.out.println(userService.findUserByFirstNameLastNameNickNameEmail("alik"));
+//            System.out.println("-------StartTest--------");
+//            BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
+//            System.out.println(bCryptPasswordEncoder.encode("adm1nadm!n"));
+//            System.out.println(bCryptPasswordEncoder.encode("!devel0per"));
+//            System.out.println(bCryptPasswordEncoder.encode("юзерИзКит@я"));
             System.out.println("----------end-----------");
         };
     }
