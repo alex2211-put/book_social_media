a392a639e6666cd6cfee3fc40337b2dba5bafae0
Alexander Putin <alik-put@yandex-team.ru>
2022-04-21 20:23:04 +0300

    init

--- Application/src/main/java/ru/iliya/controllers/ErrorController.java
+++ Application/src/main/java/ru/iliya/controllers/ErrorController.java
@@ -1,18 +1,24 @@
 package ru.iliya.controllers;
 
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+
+
 @ControllerAdvice
 public class ErrorController extends ResponseEntityExceptionHandler {
+    Logger log = LogManager.getLogger(ErrorController.class);
 
     @ExceptionHandler(Exception.class)
     @GetMapping("/error")
     public String oneException(Exception e, WebRequest web, Model model) {
         model.addAttribute("error", e.getMessage());
+        log.error(e.getMessage());
         return "error-page";
     }
 
