dd0de28b7ab723b3e07667e58df85c97116d20d5
RomanMikhailovDASR <ziroxxx3@gmail.com>
2022-04-24 15:11:44 +0300

    fix

--- DB/src/main/java/ru/iliya/services/FavouritesDataBaseServiceImpl.java
+++ DB/src/main/java/ru/iliya/services/FavouritesDataBaseServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.Date;
 import java.util.List;
 
 @Repository
-public class FavouritesBaseServiceImpl implements FavouritessDataBaseService {
+public class FavouritesDataBaseServiceImpl implements FavouritesDataBaseService {
 
     @Autowired
     private UserRepository userRepository;
